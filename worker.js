e792566ea49714f90724df76753043b2ccd59a82
kuzroman <roman.i.kuznetsov@raiffeisen.ru>
2019-01-15 19:25:39 +0300

    some

--- worker.js
+++ worker.js
@@ -1,7 +1,5 @@
 const cacheName = 'raiffeisen-site';
 
-console.log(111);
-
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
@@ -27,19 +25,65 @@ self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.open(cacheName)
+//             .then(cache => cache.match(event.request, {ignoreSearch: true}))
+//             .then(response => {
+//                 return response || fetch(event.request);
+//             })
+//     );
+// });
+
+
+
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.open(cacheName)
-            .then(cache => cache.match(event.request, {ignoreSearch: true}))
-            .then(response => {
-                return response || fetch(event.request);
+        // Этот метод анализирует запрос и
+        // ищет кэшированные результаты для этого запроса в любом из
+        // созданных сервис-воркером кэшей.
+        caches.match(event.request)
+            .then(function(response) {
+                // если в кэше найдено то, что нужно, мы можем тут же вернуть ответ.
+                if (response) {
+                    return response;
+                }
+
+                // Клонируем запрос. Так как объект запроса - это поток,
+                // обратиться к нему можно лишь один раз.
+                // При этом один раз мы обрабатываем его для нужд кэширования,
+                // ещё один раз он обрабатывается браузером, для запроса ресурсов,
+                // поэтому объект запроса нужно клонировать.
+                let fetchRequest = event.request.clone();
+
+                // В кэше ничего не нашлось, поэтому нужно выполнить загрузку материалов,
+                // что заключается в выполнении сетевого запроса и в возврате данных, если
+                // то, что нужно, может быть получено из сети.
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Проверка того, получили ли мы правильный ответ
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Клонирование объекта ответа, так как он тоже является потоком.
+                        // Так как нам надо, чтобы ответ был обработан браузером,
+                        // а так же кэшем, его нужно клонировать,
+                        // поэтому в итоге у нас будет два потока.
+                        let responseToCache = response.clone();
+
+                        caches.open(cacheName)
+                            .then(function(cache) {
+                                // Добавляем ответ в кэш для последующего использования.
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
     );
 });
-
-
-
-
 
 
 // self.addEventListener('fetch', function(event) {
