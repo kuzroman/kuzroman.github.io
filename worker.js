bbbeb467cb40b05fd78e3443925169c2a3784fef
kuzroman <roman.i.kuznetsov@raiffeisen.ru>
2019-01-16 16:53:08 +0300

    some

--- worker.js
+++ worker.js
@@ -26,42 +26,40 @@ self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim());
 });
 
-// self.addEventListener('fetch', event => {
-//     event.respondWith(
-//         caches.open(cacheName)
-//             .then(cache => cache.match(event.request, {ignoreSearch: true}))
-//             .then(response => {
-//                 return response || fetch(event.request);
-//             })
-//     );
-// });
-
-
-
-self.addEventListener('fetch', function(evt) {
-    console.log('The service worker is serving the asset.');
-
-    evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
-        return fromCache(evt.request);
-    }));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
 
-function fromNetwork(request, timeout) {
-    return new Promise(function (fulfill, reject) {
-
-        let timeoutId = setTimeout(reject, timeout);
-
-        fetch(request).then(function (response) {
-            clearTimeout(timeoutId);
-            fulfill(response);
-        }, reject);
-    });
-}
-
-function fromCache(request) {
-    return caches.open(cacheName).then(function (cache) {
-        return cache.match(request).then(function (matching) {
-            return matching || Promise.reject('no-match');
-        });
-    });
-}
+// self.addEventListener('fetch', function(evt) {
+//     console.log('The service worker is serving the asset.');
+//
+//     evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+//         return fromCache(evt.request);
+//     }));
+// });
+//
+// function fromNetwork(request, timeout) {
+//     return new Promise(function (fulfill, reject) {
+//
+//         let timeoutId = setTimeout(reject, timeout);
+//
+//         fetch(request).then(function (response) {
+//             clearTimeout(timeoutId);
+//             fulfill(response);
+//         }, reject);
+//     });
+// }
+//
+// function fromCache(request) {
+//     return caches.open(cacheName).then(function (cache) {
+//         return cache.match(request).then(function (matching) {
+//             return matching || Promise.reject('no-match');
+//         });
+//     });
+// }
