7cf92583917f5fe0d39fb5401ce7164b28bb6891
kuzroman <Roman.I.KUZNETSOV@raiffeisen.ru>
2019-04-11 19:17:54 +0300

    some

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 console.log('boom 1!');
 
-let openPopup = document.getElementById('openPopup');
+document.addEventListener('DOMContentLoaded', function(){
+
+    let openPopup = document.getElementById('openPopup');
+    let bigPopup = document.getElementById('bigPopup');
+    let closePopup = document.getElementById('closePopup');
+
+    openPopup.addEventListener('click', function () {
+        bigPopup.classList.add('show');
+    });
+    closePopup.addEventListener('click', function () {
+        bigPopup.classList.remove('show');
+    });
+
+});
+
