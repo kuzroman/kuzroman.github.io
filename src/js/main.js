554cea23f90535809b07666ab052a1394df73d38
kuzroman <Roman.I.KUZNETSOV@raiffeisen.ru>
2019-04-12 12:28:08 +0300

    hide search

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     let openPopup = document.getElementById('openPopup');
     let bigPopup = document.getElementById('bigPopup');
     let closePopup = document.getElementById('closePopup');
+    let search = document.getElementById('search');
 
     openPopup.addEventListener('click', function () {
         bigPopup.classList.add('show');
+        search.classList.add('hide');
     });
     closePopup.addEventListener('click', function () {
         bigPopup.classList.remove('show');
+        search.classList.remove('hide');
     });
 
 });
