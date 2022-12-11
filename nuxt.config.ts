c7c2130e2edc36d735e64071eac227182cd416eb
Roman Kuznetsov <kuzroman@list.ru>
2022-12-11 13:07:57 +0400

    df

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // router: {
-  //   base: '/nuxt3/'
-  // },
-  ssr: false,
-  // nitro: {
-  //   preset: 'service-worker'
-  // }
+  // ssr: false,
 })
