adfa3f555fade6f18fa4eaf8486bfebe5385733a
Roman Kuznetsov <kuzroman@list.ru>
2022-12-15 12:25:32 +0400

    update tmpl

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-<!--    <PageLoader v-if="!isPageLoaderHide" />-->
+    <PageLoader v-if="!isPageLoaderHide" />
     <MenuNavigation />
     <IconBurger />
     <SoundBar />
