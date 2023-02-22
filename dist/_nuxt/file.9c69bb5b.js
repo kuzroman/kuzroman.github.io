67b5560a96adefab5a87a49e3c7579e293d832c5
Roman Kuznetsov <kuzroman@list.ru>
2023-02-22 21:34:55 +0300

    add pwa

--- dist/_nuxt/file.9c69bb5b.js
+++
@@ -1,1 +0,0 @@
-import"./entry.b29476d1.js";const t=""+new URL("file.1bdc6af0.jpeg",import.meta.url).href;export{t as default};
--- dist/_nuxt/file.9cadc499.js
+++
@@ -1,1 +0,0 @@
-import"./entry.b29476d1.js";const t=""+new URL("file.95ec6ab9.png",import.meta.url).href;export{t as default};
