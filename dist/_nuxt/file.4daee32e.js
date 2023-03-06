86bb2d515beebd2113a5c316dd9c857892f869d0
Roman Kuznetsov <kuzroman@list.ru>
2023-03-06 19:43:52 +0300

    upd ssg

---
+++ dist/_nuxt/file.4daee32e.js
@@ -0,0 +1,1 @@
+import"./entry.d1cf4b19.js";const t=""+new URL("file.95ec6ab9.png",import.meta.url).href;export{t as default};
---
+++ dist/_nuxt/file.e3a4a5e7.js
@@ -0,0 +1,1 @@
+import"./entry.d1cf4b19.js";const t=""+new URL("file.1bdc6af0.jpeg",import.meta.url).href;export{t as default};
