f4fa7c93aed48f64c9d0a201b88b6d1da2c60f8c
Roman Kuznetsov <kuzroman@list.ru>
2023-03-06 19:43:01 +0300

    fix stars

---
+++ components/CanvasAnimation.vue
@@ -0,0 +1,16 @@
+<template>
+  <canvas class="canvas-animation"></canvas>
+</template>
+
+<script setup lang="ts">
+// const router = useRouter()
+</script>
+
+<style lang="scss">
+@import '../assets/styles/props';
+
+.canvas-animation {
+  @media (max-width: $mq-phone) {
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -23,5 +23,7 @@ useHead({
   width: 100%;
   height: 100vh;
   position: fixed;
+  left: 0;
+  top: 0;
 }
 </style>
