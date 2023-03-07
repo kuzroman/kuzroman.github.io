2c27195e8f908107fc5720a1af7faaaf6d451038
Roman Kuznetsov <kuzroman@list.ru>
2023-03-07 21:32:02 +0300

    upd static

---
+++ dist/_nuxt/file.24e6f70a.js
@@ -0,0 +1,1 @@
+import"./entry.10ca6f54.js";const t=""+new URL("file.1bdc6af0.jpeg",import.meta.url).href;export{t as default};
---
+++ dist/_nuxt/file.6543f321.js
@@ -0,0 +1,1 @@
+import"./entry.10ca6f54.js";const t=""+new URL("file.95ec6ab9.png",import.meta.url).href;export{t as default};
