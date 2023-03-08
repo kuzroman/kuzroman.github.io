ca8c8457d728a3f86617b151bea2a1a1f18d424d
Roman Kuznetsov <kuzroman@list.ru>
2023-03-08 21:02:13 +0300

    generate

--- dist/_nuxt/file.994b0d6e.js
+++
@@ -1,1 +0,0 @@
-import"./entry.ecafaa21.js";const t=""+new URL("file.95ec6ab9.png",import.meta.url).href;export{t as default};
--- dist/_nuxt/file.bfa9c825.js
+++
@@ -1,1 +0,0 @@
-import"./entry.ecafaa21.js";const t=""+new URL("file.1bdc6af0.jpeg",import.meta.url).href;export{t as default};
